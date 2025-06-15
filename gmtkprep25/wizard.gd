7016aad380aaf44d082a1c01c65a8a9e8be8cef9
Schmo103 <112767599+Schmo103@users.noreply.github.com>
2025-06-15 11:55:42 -0500

    shadows working on animations

--- gmtkprep25/wizard.gd
+++ gmtkprep25/wizard.gd
@@ -1,23 +1,28 @@
 extends Player
 
 @onready var art_scale = $wizard_art.scale.x
+@onready var shadow_art_scale = $shadow.scale.x
 @onready var anim_p = $AnimationPlayer
 
-@export var dash_power = 650
+@export var dash_power = 1300
 var dash_direction
 
 
 var dashing = false
 var anim_jumping = false
 
+
+
 func walk_right() -> void:
 	super()
 	$wizard_art.scale.x = art_scale
+	$shadow.scale.x = shadow_art_scale
 	animate("run")
 
 func walk_left() -> void:
 	super()
 	$wizard_art.scale.x = -art_scale
+	$shadow.scale.x = -shadow_art_scale
 	animate("run")
 
 
@@ -29,6 +34,15 @@ func jump() -> void:
 func handle_visuals():
 	if walk_direction == 0:
 		animate("idle")
+
+func handle_shadow():
+	if $shadow_ray.is_colliding():
+		#print("hit")
+		$shadow.global_position.y = $shadow_ray.get_collision_point().y
+		var height_ratio: float = clamp(1.0 - $shadow.position.y / 120.0, 0.2, 1.0)
+		$shadow.modulate.a = height_ratio
+		var shadow_scale: float = max(height_ratio, 0.6)
+		$shadow.scale = Vector2(shadow_scale, shadow_scale)
 
 func process_dash():
 	dashing = true
@@ -52,6 +66,8 @@ func _on_dash_timer_timeout() -> void:
 		velocity.x = sign(velocity.x) * walk_max_speed
 
 func animate(animation):
+	if is_on_floor():
+		anim_jumping = false
 	match animation:
 		"idle":
 			if !dashing:
